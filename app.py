63789668964f08e83b0be765620ffb0b4d12c069
ProPlays2312 <97733495+ProPlays2312@users.noreply.github.com>
2024-06-04 18:34:58 +0000

    app.py added

---
+++ app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import os
+from flask import Flask
+
+debug_mode = False
+
+app = Flask(__name__)
+
+@app.route('/hello_world')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=debug_mode, port = os.environ.get('PORT'))
