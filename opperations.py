f7b102b34b0289b27d21e8c303fc4c6f5e24ae50
ProPlays2312 <proplays2312@hotmail.com>
2024-09-13 00:01:12 +0530

    update 13/9 00:00

--- opperations.py
+++ opperations.py
@@ -2,28 +2,30 @@
 import pymysql
 import random
 import datetime
+import os
+from dotenv import load_dotenv
 
 timeout = 10
 connection = pymysql.connect(
   charset="utf8mb4",
   connect_timeout=timeout,
   cursorclass=pymysql.cursors.DictCursor,
-  db="database",
-  host="mysql-proplays2312-proplays2312-29cf.e.aivencloud.com",
-  password="AVNS_IqBDEspbiDiG4ZPB0qf",
+  db= os.getenv("DB_NAME"),
+  host= os.getenv("DB_HOST"),
+  password=os.getenv("DB_PASS"),
   read_timeout=timeout,
-  port=26999,
-  user="web",
+  port= int(os.getenv("DB_PORT")),
+  user=os.getenv("DB_USER"),
   write_timeout=timeout,
 )
-
+# Function to check for SQL Injection
 def check_sqli(input_string):
     sqli_payloads = ["'", "\"", ";", "--", "/*", "*/", "xp_", "exec", "sp_", "xp_cmdshell", "SELECT", "INSERT", "UPDATE", "DELETE", "DROP", "CREATE", "ALTER", "JOIN", "WHERE", "FROM", "UNION", "HAVING", "ORDER", "GROUP", "BY", "COUNT", "INTO", "VALUES", "DECLARE", "SET", "TRUNCATE", "REPLACE", "FETCH", "SUBSTRING", "CONVERT", "CAST", "HAVING", "EXISTS", "BETWEEN", "LIKE", "IN", "IS", "NULL", "AND", "OR", "NOT", "ALL", "ANY", "SOME", "CASE", "WHEN", "THEN", "ELSE", "END", "AS", "ASC", "DESC", "LIMIT", "OFFSET", "INNER", "OUTER", "LEFT", "RIGHT", "FULL", "CROSS", "NATURAL", "JOIN", "ON", "USING", "WHEN", "MATCH", "AGAINST", "LIKE", "REGEXP"]
     for i in sqli_payloads:
         if i in input_string:
             raise Exception("SQL Injection detected")
     return True
-
+# Function to register a user
 def register(username, password):
     uid = random.randint(10000,99999) ^ 12345
     date = datetime.date.today().strftime("%Y-%m-%d")
@@ -44,15 +46,13 @@ def register(username, password):
     cursor.execute(f"INSERT INTO login (u_name,uid,pass,c_date) VALUES ('{username}',{uid},'{password}','{date}');")
     connection.commit()
     return True
-
+# Function to login a user
 def login(username, password):
     try:
         if check_sqli(username) or check_sqli(password):
             pass
     except Exception as e:
         raise e
-    if not check_sqli(username) or not check_sqli(password):
-        raise Exception("SQL Injection detected")
     cursor = connection.cursor()
     cursor.execute(f"SELECT u_name,pass FROM login;")
     a = cursor.fetchall()
