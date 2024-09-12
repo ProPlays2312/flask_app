7b5f774c5dd7f2054724b9b8ab23ae8be670ee6a
ProPlays2312 <proplays2312@hotmail.com>
2024-09-12 22:53:19 +0530

    commit 12/9 22:53

--- app_dev.py
+++ app_dev.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index2.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
--- opperations.py
+++ opperations.py
@@ -17,7 +17,19 @@ connection = pymysql.connect(
   write_timeout=timeout,
 )
 
+def check_sqli(input_string):
+    sqli_payloads = ["'", "\"", ";", "--", "/*", "*/", "xp_", "exec", "sp_", "xp_cmdshell", "SELECT", "INSERT", "UPDATE", "DELETE", "DROP", "CREATE", "ALTER", "JOIN", "WHERE", "FROM", "UNION", "HAVING", "ORDER", "GROUP", "BY", "COUNT", "INTO", "VALUES", "DECLARE", "SET", "TRUNCATE", "REPLACE", "FETCH", "SUBSTRING", "CONVERT", "CAST", "HAVING", "EXISTS", "BETWEEN", "LIKE", "IN", "IS", "NULL", "AND", "OR", "NOT", "ALL", "ANY", "SOME", "CASE", "WHEN", "THEN", "ELSE", "END", "AS", "ASC", "DESC", "LIMIT", "OFFSET", "INNER", "OUTER", "LEFT", "RIGHT", "FULL", "CROSS", "NATURAL", "JOIN", "ON", "USING", "WHEN", "MATCH", "AGAINST", "LIKE", "REGEXP"]
+    for i in sqli_payloads:
+        if i in input_string:
+            raise Exception("SQL Injection detected")
+    return True
+
 def register(username, password):
+    try:
+        if check_sqli(username) or check_sqli(password):
+            pass
+    except Exception as e:
+        raise e
     uid = random.randint(10000,99999) ^ 12345
     date = datetime.date.today().strftime("%Y-%m-%d")
     cursor = connection.cursor()
@@ -31,13 +43,21 @@ def register(username, password):
             continue
     for i in a:
         if i["uid"] == uid:
-            raise Exception("UID already exists")
+            return register(username, password)
         else:
             continue
     cursor.execute(f"INSERT INTO login (u_name,uid,pass,c_date) VALUES ('{username}',{uid},'{password}','{date}');")
     connection.commit()
     return True
+
 def login(username, password):
+    try:
+        if check_sqli(username) or check_sqli(password):
+            pass
+    except Exception as e:
+        raise e
+    if not check_sqli(username) or not check_sqli(password):
+        raise Exception("SQL Injection detected")
     cursor = connection.cursor()
     cursor.execute(f"SELECT u_name,pass FROM login;")
     a = cursor.fetchall()
